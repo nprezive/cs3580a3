2daa68f1d47310176a6def9b8949ad2caf625055
Noah Preszler <npreszler@gmail.com>
2018-02-10 20:36:32 -0700

    Added a .py for testing out new stuff. Added a histogram example. Added a .gitignore. Worked through the assignment up to halfway through the univariate values.

--- a3.py
+++ a3.py
@@ -1,37 +1,68 @@
 #import matplotlib
 from collections import defaultdict
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def basicInfo():
-    print("Basic information")
-    print()
-    print("\tNumber of chocolates per rating")
+df = pd.read_csv("flavors_of_cacao.csv", 
+            names=[
+                'Company',
+                'Specific Origin',
+                'REF',
+                'Review Date',
+                'Cacao Percent',
+                'Company Location',
+                'Rating',
+                'Bean Type',
+                'Broad Origin'],
+            header=0,
+            converters={
+                4: lambda p: float(p.strip('%'))/100
+            })
 
-    barCountByRating = defaultdict(int)
 
-    with open("flavors_of_cacao.csv") as f:
-        next(f)
-        for line in f:
-            lineList = line.split(",")
-            print(lineList)
-            rating = lineList[6]
-            barCountByRating[rating] += 1
+def chocolateRatings():
+    ratings = df['Rating']
+    ratingsDict = defaultdict(int)
+    for r in ratings:
+        ratingsDict[r] += 1
+
+    print("Number of chocolates per rating:")
+    for k in sorted(ratingsDict.keys(), reverse=True):
+        print("\t{0}: {1}".format(k, ratingsDict[k]))
+    plt.bar(list(ratingsDict.keys()), list(ratingsDict.values()), color="g")
+    plt.title("Number of chocolates per rating")
+    plt.xlabel("Ratings")
+    plt.ylabel("# of Chocolates")
+    plt.show()
+
+
+def top5Countries():
+    meanRatingByCountry = df.groupby(['Company Location'], as_index=False)[['Rating']].mean() \
+                            .sort_values('Rating', ascending=False) \
+                            .rename(columns={'Rating': 'Mean Rating'})
     
-    print(barCountByRating)
+    print('\nTop 5 countries:  (highest mean rating)')
+    print(meanRatingByCountry.head(5).to_string(index=False))
 
 
-def pandasTest():
-    df = pd.read_csv("flavors_of_cacao.csv")
-    df = df.rename(columns={df.columns[0]:"Company",
-                                df.columns[1]:"Specific Origin", 
-                                df.columns[3]:"Review Date", 
-                                df.columns[4]:"Cocoa Percent", 
-                                df.columns[5]:"Company Location", 
-                                df.columns[7]:"Bean Type", 
-                                df.columns[8]:"Broad Origin"})
-    print(df)
+def univariateValues():
+    print('\nUnivariate values:')
+    
+    # Mean cacao percentage
+    meanCacao = df['Cacao Percent'].mean()
+    print('\tMean cacao percentage: {0:.1f}%'.format(meanCacao*100))
+
+    # Range of cacao percentage
+    rangeCacao = df['Cacao Percent'].max() - df['Cacao Percent'].min()
+    print('\tRange of caco percentage: {0:.1f}%'.format(rangeCacao*100))
+
+    
+
+
 
 # Run program
-# basicInfo()
-pandasTest()
+# chocolateRatings()
+top5Countries()
+univariateValues()
---
+++ goof.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.random.random_integers(0,10,20)
+plt.hist(a)
+plt.show()
