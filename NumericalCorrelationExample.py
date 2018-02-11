b8742be0918e1670f98a6ac7b59060d3365b39a4
Noah Preszler <npreszler@gmail.com>
2018-02-11 00:20:08 -0700

    Finished the univariate values. Finished the first correlation.

--- NumericalCorrelationExample.py
+++ NumericalCorrelationExample.py
@@ -29,6 +29,7 @@ webbrowser.open('file://' + os.path.realpath("tableExample.html"))
 plt.rcParams['figure.figsize'] = [16, 6]
 
 fig, ax = plt.subplots(nrows=1, ncols=3)
+print("fig: " + str(fig))
 
 ax=ax.flatten()
 
--- a3.py
+++ a3.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("flavors_of_cacao.csv", 
             names=[
                 'Company',
-                'Specific Origin',
+                'Specific_Origin',
                 'REF',
-                'Review Date',
-                'Cacao Percent',
-                'Company Location',
+                'Review_Date',
+                'Cacao_Percent',
+                'Company_Location',
                 'Rating',
-                'Bean Type',
-                'Broad Origin'],
+                'Bean_Type',
+                'Broad_Origin'],
             header=0,
             converters={
                 4: lambda p: float(p.strip('%'))/100
@@ -39,7 +39,7 @@ def chocolateRatings():
 
 
 def top5Countries():
-    meanRatingByCountry = df.groupby(['Company Location'], as_index=False)[['Rating']].mean() \
+    meanRatingByCountry = df.groupby(['Company_Location'], as_index=False)[['Rating']].mean() \
                             .sort_values('Rating', ascending=False) \
                             .rename(columns={'Rating': 'Mean Rating'})
     
@@ -51,18 +51,47 @@ def univariateValues():
     print('\nUnivariate values:')
     
     # Mean cacao percentage
-    meanCacao = df['Cacao Percent'].mean()
+    meanCacao = df['Cacao_Percent'].mean()
     print('\tMean cacao percentage: {0:.1f}%'.format(meanCacao*100))
 
     # Range of cacao percentage
-    rangeCacao = df['Cacao Percent'].max() - df['Cacao Percent'].min()
+    rangeCacao = df['Cacao_Percent'].max() - df['Cacao_Percent'].min()
     print('\tRange of caco percentage: {0:.1f}%'.format(rangeCacao*100))
+
+    # Mode of Bean Type
+    beanTypeMode = (df[df.Bean_Type != '\xa0'])['Bean_Type'].mode()
+    print('\tMode of bean type: {0}'.format(beanTypeMode.to_string(index=False)))
+
+    # Mode of Broad Origin
+    broadOriginMode = df['Broad_Origin'].mode()
+    print('\tMode of broad bean origin: {0}'.format(broadOriginMode.to_string(index=False)))
+
+    # Mode of Company Location
+    companyLocationMode = df['Company_Location'].mode()
+    print('\tMode of company location: {0}'.format(companyLocationMode.to_string(index=False)))
+
+
+def correlations():
+    print('\nCorrelations:')
+
+    corrPercentCacaoAndRating = df['Cacao_Percent'].corr(df['Rating'])
+    print('\tCorrelation between percent of cacao and rating: {0:.3f}'.format(corrPercentCacaoAndRating))
+    #scatter plot
+    plt.title('Correlation between percent of cacao and rating')
+    plt.xlabel('% Cacao')
+    plt.ylabel('Rating')
+    #regression line
+    fit = np.polyfit(df['Cacao_Percent'], df['Rating'], 1)
+    fit_fn = np.poly1d(fit) 
+    plt.plot(df['Cacao_Percent'], df['Rating'], 'yo', df['Cacao_Percent'], fit_fn(df['Cacao_Percent']), '--k')
+    plt.show()
+    print("\tWith a correlation coefficient so small, it looks like there's no correlation between the two.")
 
     
 
 
-
 # Run program
 # chocolateRatings()
-top5Countries()
-univariateValues()
+# top5Countries()
+# univariateValues()
+correlations()
